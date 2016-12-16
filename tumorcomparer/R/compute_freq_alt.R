ad4f42c37feeb26f2d7704a1012b0c9938d23f21
Rileen Sinha <rileen@gmail.com>
2016-12-16 18:00:01 -0500

    After adding some documentation

--- tumorcomparer/R/compute_freq_alt.R
+++ tumorcomparer/R/compute_freq_alt.R
@@ -1,3 +1,13 @@
+#'Computes frequency of alteration (fraction of non-zeroes)
+#' @alt_mat a vector of discrete values - 0s represent unaltered, non-zeroes altered
+#' 
+#'
+#' @author Rileen Sinha (rileen@gmail.com), Augustin Luna (augustin@mail.nih.gov)
+#'
+#' 
+#'
+#' @concept tumorcomparer
+#' @export
 compute_freq_alt <- function(alt_mat) {
-  return(length(which(alt_mat!=0))/length(alt_mat))
+  return(length(which(alt_mat!=0))/length(alt_mat)) # fraction of non-zeroes, ie altered features/genes
 }
--- tumorcomparer/R/keep_only_high_level_cnas.R
+++ tumorcomparer/R/keep_only_high_level_cnas.R
@@ -1,6 +1,19 @@
-#' X 
+#' Keep only high-level CNAs (set low-level CNAs to 0)
+#' @cna_mat a vector of 5-valued CNA (copy number alteration) data - 
+#' -2: Deep deletion
+#' -1: Shallow deletion
+#' 0: Diploid (or default copy number)
+#' 1: Low-level gain
+#' 2: High-level amplification
+#'
+#' @author Rileen Sinha (rileen@gmail.com), Augustin Luna (augustin@mail.nih.gov)
+#'
+#' 
+#'
+#' @concept tumorcomparer
+#' @export
 keep_only_high_level_cnas <- function(cna_mat) {
-  cna_mat_high_only <- cna_mat
-  cna_mat_high_only[which(abs(cna_mat) == 1)] <- 0
-  return(cna_mat_high_only)
+  cna_mat_high_only <- cna_mat # copy input matrix
+  cna_mat_high_only[which(abs(cna_mat) == 1)] <- 0 # set low-level CNAs in copy to 0
+  return(cna_mat_high_only) # return copy
 }
