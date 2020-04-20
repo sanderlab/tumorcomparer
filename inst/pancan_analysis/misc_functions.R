853b1ec00a08dadcaab7285326b5f0b4c92cb6e4
cannin <cannin@gmail.com>
2020-04-20 11:17:12 -0400

    Added misc functions

--- inst/pancan_analysis/misc_functions.R
+++ inst/pancan_analysis/misc_functions.R
@@ -6,8 +6,16 @@ return_first_part_Bar <- function(x){
   return(strsplit(x,"\\|")[[1]][1])
 }
 
+return_fix_TCGA_ID_dashes <- function(x){
+  if(grepl(".", x)) {
+    x <- gsub("\\.", "-", x)
+  }
+
+  return(x)
+}
+
 return_3_field_TCGA_ID <- function(x){
-  return( paste(strsplit((x),"-")[[1]][1],str_split((x),"-")[[1]][2],str_split((x),"-")[[1]][3],sep="-"))
+  return(paste(strsplit((x),"-")[[1]][1], strsplit((x),"-")[[1]][2], strsplit((x),"-")[[1]][3],sep="-"))
 }
 
 return_first_part <- function(x){ return(strsplit(x,"_")[[1]][1]) }
