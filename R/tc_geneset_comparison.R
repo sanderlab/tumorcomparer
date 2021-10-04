56c6208200c81ebc6c3075350efd236c863d9825
cannin <cannin@gmail.com>
2021-10-04 17:38:31 -0400

    Create vignette showing how to process TC dataset Added code to select specific IDs from TC dataset

--- R/tc_geneset_comparison.R
+++ R/tc_geneset_comparison.R
@@ -3,6 +3,8 @@
 #' @param gene_list a vector of HGNC gene symbols which will be used to perform the comparison
 #' @param cancer_type TCGA cancer name (GBM, LGG, etc.)
 #' @param tc_dataset path to RData for TumorComparer dataset from publication; see details
+#' @param tcga_ids a vector of TCGA IDs for analysis; cannot be combined with cancer_type
+#' @param cclp_ids a vector of CCLP IDs for analysis; cannot be combined with cancer_type
 #' @param tc_dataset_dir path to directory with TumorComparer dataset as text files (recommended instead of tc_dataset); see details
 #' @param remove_errored_dataset_comparisons will skip the data types which can't be compared for technical reasons(no enaugh genes to compare, or data contain only 0 values) when set to TRUE (Default: FALSE)
 #' @param remove_tmp_files remove temporary files (Default: TRUE)
@@ -25,12 +27,16 @@
 #' @importFrom data.table fwrite fread
 #' 
 #' @export 
-tc_geneset_comparison <- function(gene_list, cancer_type, 
-                                    tc_dataset=NULL, 
-                                    tc_dataset_dir=NULL, 
-                                    remove_errored_dataset_comparisons=FALSE, 
-                                    remove_tmp_files=TRUE,
-                                    verbose=FALSE) {
+tc_geneset_comparison <- function(gene_list, 
+                                  cancer_type=NULL, 
+                                  tcga_ids=NULL,
+                                  cclp_ids=NULL, 
+                                  tc_dataset=NULL, 
+                                  tc_dataset_dir=NULL,
+                                  remove_errored_dataset_comparisons=FALSE, 
+                                  remove_tmp_files=TRUE,
+                                  verbose=FALSE) {
+  
   # FROM: https://www.r-bloggers.com/2019/08/no-visible-binding-for-global-variable/
   # to deal with "undefined global functions or variables" errors
   CCLP_Expression_Quantile_Normalized <- NULL
@@ -65,34 +71,51 @@ tc_geneset_comparison <- function(gene_list, cancer_type,
     CCLP_TCGA_Types_Combined <- fread(input=file.path(tc_dataset_dir, "CCLP_ID_and_Cancer_Type.txt"))
     TCGA_id_and_tumor_type <- fread(input=file.path(tc_dataset_dir, "TCGA_ID_and_Cancer_Type.txt"))
   } else if(!is.null(tc_dataset)) {
-    load(tc_dataset)  
+    load(tc_dataset)
   } else {
     stop("ERROR: TumorComparer dataset location was not specified")
   }
   
-  if(length(cancer_type) > 1) {
-    stop("ERROR: The analysis can be run only for one cancer type")  
+  # TODO: Create test for this conditions
+  if(is.null(cancer_type) && !is.null(tcga_ids) && !is.null(cclp_ids)) {
+    tmp_mut_cclp_ids <- cclp_ids
+    tmp_mut_tcga_ids <- tcga_ids
+    tmp_cna_cclp_ids <- cclp_ids
+    tmp_cna_tcga_ids <- tcga_ids
+    tmp_exp_cclp_ids <- cclp_ids
+    tmp_exp_tcga_ids <- tcga_ids
+  } else if(!is.null(cancer_type) && is.null(tcga_ids) && is.null(cclp_ids)) {
+    if(length(cancer_type) > 1) {
+      stop("ERROR: The analysis can be run only for one cancer type")  
+    }
+    
+    most_variable_genes_precomputed_path <- system.file("extdata", 
+                                                        "mtc_results_20200331", 
+                                                        "mtc_results_20200331_no_factors.rds", 
+                                                        package = "tumorcomparer")
+    most_variable_genes_precomputed_results <- readRDS(most_variable_genes_precomputed_path)
+    avail_cancer_types <- unique(most_variable_genes_precomputed_results$Tumor_Cancer_Type)
+    
+    if(!(cancer_type %in% avail_cancer_types)) {
+      stop("ERROR: TCGA cancer type with specified name is not found")
+    }
+    
+    tmp_mut_cclp_ids <- intersect(colnames(mut_mat_CCLP_after_Annovar), CCLP_TCGA_Types_Combined$Model_name[which(CCLP_TCGA_Types_Combined$TCGA_Type == cancer_type)])
+    tmp_mut_tcga_ids <- intersect(colnames(mut_mat_TCGA_after_Annovar), TCGA_id_and_tumor_type$TCGA_ID[which(TCGA_id_and_tumor_type$Cancer_Type == cancer_type)])
+    tmp_cna_cclp_ids <- intersect(colnames(CCLP_GISTIC_all_data_by_genes), CCLP_TCGA_Types_Combined$Model_name[which(CCLP_TCGA_Types_Combined$TCGA_Type == cancer_type)])
+    tmp_cna_tcga_ids <- intersect(colnames(TCGA_GISTIC_all_data_by_genes), TCGA_id_and_tumor_type$TCGA_ID[which(TCGA_id_and_tumor_type$Cancer_Type == cancer_type)])
+    tmp_exp_cclp_ids <- intersect(colnames(CCLP_Expression_Quantile_Normalized), CCLP_TCGA_Types_Combined$Model_name[which(CCLP_TCGA_Types_Combined$TCGA_Type == cancer_type)])
+    tmp_exp_tcga_ids <- intersect(colnames(TCGA_Expression_Quantile_Normalized), TCGA_id_and_tumor_type$TCGA_ID[which(TCGA_id_and_tumor_type$Cancer_Type == cancer_type)])
+  } else {
+    stop("ERROR: Either (cancer_type) OR (tcga_ids and cclp_ids) must be set, but not both.")
   }
   
-  most_variable_genes_precomputed_path <- system.file("extdata", 
-                                                      "mtc_results_20200331", 
-                                                      "mtc_results_20200331_no_factors.rds", 
-                                                      package = "tumorcomparer")
-  most_variable_genes_precomputed_results <- readRDS(most_variable_genes_precomputed_path)
-  avail_cancer_types <- unique(most_variable_genes_precomputed_results$Tumor_Cancer_Type)
-  
-  if(!(cancer_type %in% avail_cancer_types)) {
-    stop("ERROR: TCGA cancer type with specified name is not found")
-  }
-  
-  intersect(colnames(mut_mat_CCLP_after_Annovar), CCLP_TCGA_Types_Combined$Model_name[which(CCLP_TCGA_Types_Combined$TCGA_Type == cancer_type)])
-  
-  mut_cclp_ids <- c("Gene", intersect(colnames(mut_mat_CCLP_after_Annovar), CCLP_TCGA_Types_Combined$Model_name[which(CCLP_TCGA_Types_Combined$TCGA_Type == cancer_type)]))
-  mut_tcga_ids <- c("Gene", intersect(colnames(mut_mat_TCGA_after_Annovar), TCGA_id_and_tumor_type$TCGA_ID[which(TCGA_id_and_tumor_type$Cancer_Type == cancer_type)]))
-  cna_cclp_ids <- c("Gene", intersect(colnames(CCLP_GISTIC_all_data_by_genes), CCLP_TCGA_Types_Combined$Model_name[which(CCLP_TCGA_Types_Combined$TCGA_Type == cancer_type)]))
-  cna_tcga_ids <- c("Gene", intersect(colnames(TCGA_GISTIC_all_data_by_genes), TCGA_id_and_tumor_type$TCGA_ID[which(TCGA_id_and_tumor_type$Cancer_Type == cancer_type)]))
-  exp_cclp_ids <- c("Gene", intersect(colnames(CCLP_Expression_Quantile_Normalized), CCLP_TCGA_Types_Combined$Model_name[which(CCLP_TCGA_Types_Combined$TCGA_Type == cancer_type)]))
-  exp_tcga_ids <- c("Gene", intersect(colnames(TCGA_Expression_Quantile_Normalized), TCGA_id_and_tumor_type$TCGA_ID[which(TCGA_id_and_tumor_type$Cancer_Type == cancer_type)]))
+  mut_cclp_ids <- c("Gene", tmp_mut_cclp_ids)
+  mut_tcga_ids <- c("Gene", tmp_mut_tcga_ids)
+  cna_cclp_ids <- c("Gene", tmp_cna_cclp_ids)
+  cna_tcga_ids <- c("Gene", tmp_cna_tcga_ids)
+  exp_cclp_ids <- c("Gene", tmp_exp_cclp_ids)
+  exp_tcga_ids <- c("Gene", tmp_exp_tcga_ids)
   
   if(sum(gene_list %in% Reduce(intersect, list(mut_mat_TCGA_after_Annovar$Gene, TCGA_GISTIC_all_data_by_genes$Gene, TCGA_Expression_Quantile_Normalized$Gene))) < 2) {
     stop("ERROR: The number of genes for comparison is less than 2")
@@ -138,5 +161,4 @@ tc_geneset_comparison <- function(gene_list, cancer_type,
   }
 
   return(comparison_list)
-  
 }
