873f14420d370b81a9c82dc594468e855dfc5717
cannin <cannin@gmail.com>
2022-05-19 10:22:55 -0400

    Added specific mutations for cell lines to static pages Call oncoprint code from GitHub Move CCLE mapping to file for reuse

--- inst/static_page_generation/generate_tumorcomparer_static_rmds.R
+++ inst/static_page_generation/generate_tumorcomparer_static_rmds.R
@@ -1,5 +1,11 @@
 library(brew)
 library(rmarkdown)
+library(stringr)
+
+# work_dir <- "inst/static_page_generation/"
+# setwd(work_dir)
+
+source("https://raw.githubusercontent.com/cannin/cbioportal_to_complexheatmap_oncoprint/master/transform_cbioportal_to_complexheatmap_oncoprint.R")
 
 # Parameters 
 labels <- c("mRNA High", "mRNA Low", "Protein High",
@@ -25,7 +31,7 @@ create.report <- function(reports, prepend=NULL) {
   # Set parameters 
   file_prefix <- reports$tcga_abbrev
 
-  tcga_title <- reports$tcga_title
+  tcga_title <- str_to_title(reports$tcga_title)
   tcga_abbrev <- reports$tcga_abbrev
   
   tmp_oncoprint_caption <- paste0(c("Comparing", tolower(tcga_title), "cell lines to tumor samples."), collapse=" ")
@@ -46,24 +52,9 @@ create.report <- function(reports, prepend=NULL) {
 files <- dir(".", "^[a-z]*_cclp_tcga_tumorcomparer.Rmd")
 file.remove(files)
 
-tcga_types <- c(
-  "Breast Cancer"="BRCA",
-  "Colon Cancer"="COAD",
-  "Esophageal Cancer"="ESCA",
-  "Kidney Cancer"="KIRC",
-  "Liver Hepatocellular Cancer"="LIHC", 
-  "Lung Cancer"="LUAD",   
-  #"Mesothelioma"="MESO",
-  "Ovarian Cancer"="OV",
-  "Pancreatic Cancer"="PAAD",
-  "Prostate Cancer"="PRAD", 
-  "Skin Cancer"="SKCM",
-  "Stomach Cancer"="STAD",
-  "Thyroid Cancer"="THCA",
-  "Endometrial Cancer"="UCEC"
-)
+ccle_tcga_mapping <- read.table("ccle_tcga_mapping.txt", sep="\t", header=TRUE, stringsAsFactors=FALSE)
 
-reports <- data.frame(tcga_abbrev=tolower(tcga_types), tcga_title=names(tcga_types), stringsAsFactors=FALSE)
+reports <- data.frame(tcga_abbrev=tolower(ccle_tcga_mapping$tcga), tcga_title=ccle_tcga_mapping$name, stringsAsFactors=FALSE)
 
 # GENERATE REPORTS ----
 for(i in 1:nrow(reports)) {
@@ -82,7 +73,6 @@ writeLines(paste0(base_url, file_list), file.path(output_dir, "sitemap.txt"))
 #source("generate_tumorcomparer_static_rmds.R")
 
 # CLEANUP ----
-tmp <- list.files(pattern="*_tcga_tumorcomparer.Rmd", full.names=TRUE)
-file.remove(tmp)
-
+file.remove(list.files(pattern="*_tcga_tumorcomparer.[Rmd|html]", full.names=TRUE))
+#file.remove(list.files(pattern="*_tcga_tumorcomparer", full.names=TRUE))
 
